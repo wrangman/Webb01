1955690664565719b3d67fd9dcbba65476f9ab23
wrangman <wrangman@gmail.com>
2022-09-17 17:02:01 +0200

    räknaren klar

--- flex.js
+++ flex.js
@@ -1,3 +1,4 @@
+var buttonAvail = false;
 var selColor1 = null;
 var selColor2 = null;
 let tempQuery = window.location.search;
@@ -37,8 +38,6 @@ function aboutDialog() {
 }
 
 function showSubMenu(whatId, stateShow) {
-    console.log(stateShow);
-
     if (stateShow == true) {
         document.getElementById(whatId).style.visibility = "visible";
     } else {
@@ -93,3 +92,80 @@ function btnEval() {
         selColor2 = null
     }
 }
+function btnCalc(whichButton) {
+    let calcInput = document.getElementById(whichButton).innerHTML;
+    let calcScreen = document.getElementById('calc-screen').innerHTML;
+    let screenLength = calcScreen.length;
+
+    switch (whichButton) {                      //Vilken knapp tryckte användaren på?
+        case 'btn4':                            //Dela
+            if (buttonAvail) inputNumbers('/');
+            break;
+        case 'btn8':                            //Gånger
+            if (buttonAvail) inputNumbers('*');
+            break;
+        case 'btn12':                            //Minus
+            if (buttonAvail) inputNumbers('-');
+            break;
+        case 'btn13':                            //Convert
+            if (buttonAvail) convert();
+            break;
+        case 'btn16':                            //Plus
+            if (buttonAvail) inputNumbers('+');
+            break;
+        case 'btn18':                           //Sudda
+            backSpace();
+            break;
+        case 'btn17':                           //Sudda allt
+            clearScreen();
+            break;
+        case 'btn19':                            //Minus
+            if (buttonAvail) calculate();
+            break;
+        default:                                //Nuffror
+            inputNumbers(calcInput);
+    } //switch
+
+    function inputNumbers(whatDigit) {
+        if (!buttonAvail) clearScreen();
+        buttonAvail=true;
+        if (screenLength<19) {
+            document.getElementById('calc-screen').innerHTML += whatDigit;
+        }
+    }
+
+    function backSpace() {
+        if (screenLength > 0) {
+            calcScreen = calcScreen.slice(0, -1);
+            document.getElementById('calc-screen').innerHTML = calcScreen;
+        } else {
+            buttonAvail=false;
+        }
+    }
+
+    function convert() {
+        if (calcScreen <0 ) {
+            calcScreen = Math.abs(calcScreen);
+            document.getElementById('calc-screen').innerHTML = calcScreen;
+        } else if (calcScreen > 0) {
+            calcScreen = calcScreen * -1;
+            document.getElementById('calc-screen').innerHTML = calcScreen;
+        }
+    }
+
+    function calculate() {
+        try {
+            let result = eval(calcScreen);
+            if (result != "Infinity" && result != "-Infinity") document.getElementById('calc-screen').innerHTML = result;
+        } catch (error) {
+            alert("Felaktig inmatning");
+        }
+    }
+
+    function clearScreen() {
+        buttonAvail=false;
+        document.getElementById('calc-screen').innerHTML = "";
+    }
+}
+
+
