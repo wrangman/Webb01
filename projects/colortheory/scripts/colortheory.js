284897fa83a3b8245f4c4dda3217fbbb8e3ac8c7
wrangman <wrangman@gmail.com>
2022-09-26 22:20:13 +0200

    fixing the thing

---
+++ projects/colortheory/scripts/colortheory.js
@@ -0,0 +1,47 @@
+document.querySelectorAll('.color-link').forEach(item => {
+    item.addEventListener('click', event => {
+      
+      alert(item);
+        
+    })
+  }) 
+
+/*
+  const slice1 = document.getElementById("clr-slice1-link")
+  const slice2 = document.getElementById("clr-slice2-link")
+  const slice3 = document.getElementById("clr-slice3-link")
+  const slice4 = document.getElementById("clr-slice4-link")
+  const slice5 = document.getElementById("clr-slice5-link")
+  const slice6 = document.getElementById("clr-slice6-link")
+  const slice7 = document.getElementById("clr-slice7-link")
+  const slice8 = document.getElementById("clr-slice8-link")
+  const slice9 = document.getElementById("clr-slice9-link")
+  const slice10 = document.getElementById("clr-slice10-link")
+  const slice11 = document.getElementById("clr-slice11-link")
+  const slice12 = document.getElementById("clr-slice12-link")
+  */
+
+  /*
+
+  slice1.addEventListener("click", function() {
+    console.log("1");
+  });
+
+  slice2.addEventListener("click", function() {
+    console.log("2");
+  });
+
+  slice3.addEventListener("click", function() {
+    console.log("3");
+  });
+*/
+  
+
+/*
+const slice1 = document.querySelector('.hold');
+
+slice1.addEventListener('click', e => function() {
+    alert("slice1");
+});
+
+*/
