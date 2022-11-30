c1bab12e39a29d8d2032853d830a75735ab807ea
wrangman <wrangman@gmail.com>
2022-11-30 15:46:44 +0100

    hihi

--- scripts/automenu.js
+++ scripts/automenu.js
@@ -1,15 +1,19 @@
 
 const header = document.querySelector("header");
-const navBackground = document.querySelector(".navBackground");
+const navBackground = document.querySelector("nav");
 const containerOptions = {};
 
 const containerObserver = new IntersectionObserver(function(
     entries, 
-    containerObserver
+    appearOnScroll
 ) { 
     entries.forEach(entry => {
         if(!entry.isIntersecting) {
+            return;
+        } else {
+            
             header.classList.add("scrolled");
+            appearOnScroll.unobserve(entry.target);
         }
     })
 
